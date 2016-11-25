defb313f692448e9737adb52cf64d789dd6739ec
hani <jhanihashemi@gmail.com>
2016-11-26 02:12:53 +0330

    Google ads removed from all pages

--- app/src/main/java/com/kbeanie/multipicker/sample/AbActivity.java
+++ app/src/main/java/com/kbeanie/multipicker/sample/AbActivity.java
@@ -6,17 +6,12 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.kbeanie.multipicker.sample.prefs.AppPreferences;
 
 /**
  * Created by kbibek on 2/18/16.
  */
 public class AbActivity extends AppCompatActivity {
-
-    private AdView adView;
-
     protected AppPreferences preferences;
 
     @Override
@@ -29,12 +24,6 @@ public class AbActivity extends AppCompatActivity {
         preferences = new AppPreferences(this);
     }
 
-    @Override
-    public void setContentView(@LayoutRes int layoutResID) {
-        super.setContentView(layoutResID);
-        setupAds();
-    }
-
     public final static String GALAXY_TAB = "6B7B033AC9940497E369C02B714E9483";
     public final static String NEXUS_S = "55958F02BF66EEC31424761A58B1733B";
     public final static String TEST_DEVICE_ID_2 = "79B7F70DBE55777CD06F8FE2EBEB92A1";
@@ -44,27 +33,6 @@ public class AbActivity extends AppCompatActivity {
     public final static String XIAOMI = "EEAEE535D859D6F87E56B88B7A65D984";
 
     private final static String[] TEST_DEVICES = {GALAXY_TAB, NEXUS_S, TEST_DEVICE_ID_2, TEST_GALAXY_NEXUS, TEST_OPO, TEMP_DEVICE, XIAOMI};
-
-    protected void setupAds() {
-        adView = (AdView) findViewById(R.id.adView);
-
-        AdRequest.Builder builder = new AdRequest.Builder();
-        builder.addTestDevice(AdRequest.DEVICE_ID_EMULATOR);
-
-        for (String device : TEST_DEVICES) {
-            builder.addTestDevice(device);
-        }
-        AdRequest request = builder.build();
-        adView.loadAd(request);
-    }
-
-    @Override
-    public void onDestroy() {
-        if (adView != null) {
-            adView.destroy();
-        }
-        super.onDestroy();
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
