ac792f6576db4e6ed8e68a397a1e6584c3bdb8c9
hani <jhanihashemi@gmail.com>
2017-01-02 16:58:52 +0330

    Remove unused class

--- app/src/main/java/com/kbeanie/multipicker/sample/HomeActivity.java
+++ app/src/main/java/com/kbeanie/multipicker/sample/HomeActivity.java
@@ -1,14 +1,9 @@
 package com.kbeanie.multipicker.sample;
 
-import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,6 +17,8 @@ import com.kbeanie.multipicker.sample.adapters.DemosAdapter;
  * Created by kbibek on 2/18/16.
  */
 public class HomeActivity extends AbActivity implements AdapterView.OnItemClickListener {
+
+    private int[] IMAGE_OPTIONS = {DemosAdapter.IMAGE_PICKER_ACTIVITY, DemosAdapter.IMAGE_PICKER_FRAGMENT, DemosAdapter.IMAGE_PICKER_SUPPORT_FRAGMENT};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -77,39 +74,6 @@ public class HomeActivity extends AbActivity implements AdapterView.OnItemClickL
             case DemosAdapter.IMAGE_PICKER_SUPPORT_FRAGMENT:
                 intent = new Intent(this, ImagePickerSupportFragmentActivity.class);
                 break;
-            case DemosAdapter.VIDEO_PICKER_ACTIVITY:
-                intent = new Intent(this, VideoPickerActivity.class);
-                break;
-            case DemosAdapter.VIDEO_PICKER_FRAGMENT:
-                intent = new Intent(this, VideoPickerFragmentActivity.class);
-                break;
-            case DemosAdapter.VIDEO_PICKER_SUPPORT_FRAGMENT:
-                intent = new Intent(this, VideoPickerSupportFragmentActivity.class);
-                break;
-            case DemosAdapter.FILE_PICKER_ACTIVITY:
-                intent = new Intent(this, FilePickerActivity.class);
-                break;
-            case DemosAdapter.FILE_PICKER_FRAGMENT:
-                intent = new Intent(this, FilePickerFragmentActivity.class);
-                break;
-            case DemosAdapter.FILE_PICKER_SUPPORT_FRAGMENT:
-                intent = new Intent(this, FilePickerSupportFragmentActivity.class);
-                break;
-            case DemosAdapter.CONTACT_PICKER_ACTIVITY:
-                intent = new Intent(this, ContactPickerActivity.class);
-                break;
-            case DemosAdapter.AUDIO_PICKER_ACTIVITY:
-                intent = new Intent(this, AudioPickerActivity.class);
-                break;
-            case DemosAdapter.AUDIO_PICKER_FRAGMENT:
-                intent = new Intent(this, AudioPickerFragmentActivity.class);
-                break;
-            case DemosAdapter.AUDIO_PICKER_SUPPORT_FRAGMENT:
-                intent = new Intent(this, AudioPickerSupportFragmentActivity.class);
-                break;
-            case DemosAdapter.MEDIA_PICKER_ACTIVITY:
-                intent = new Intent(this, MediaPickerActivity.class);
-                break;
         }
 
         if (intent != null) {
@@ -117,21 +81,10 @@ public class HomeActivity extends AbActivity implements AdapterView.OnItemClickL
         }
     }
 
-    private int[] IMAGE_OPTIONS = {DemosAdapter.IMAGE_PICKER_ACTIVITY, DemosAdapter.IMAGE_PICKER_FRAGMENT, DemosAdapter.IMAGE_PICKER_SUPPORT_FRAGMENT};
-    private int[] VIDEO_OPTIONS = {DemosAdapter.VIDEO_PICKER_ACTIVITY, DemosAdapter.VIDEO_PICKER_FRAGMENT, DemosAdapter.VIDEO_PICKER_SUPPORT_FRAGMENT};
-    private int[] FILE_OPTIONS = {DemosAdapter.FILE_PICKER_ACTIVITY, DemosAdapter.FILE_PICKER_FRAGMENT, DemosAdapter.FILE_PICKER_SUPPORT_FRAGMENT};
-    private int[] AUDIO_OPTIONS = {DemosAdapter.AUDIO_PICKER_ACTIVITY, DemosAdapter.AUDIO_PICKER_FRAGMENT, DemosAdapter.AUDIO_PICKER_SUPPORT_FRAGMENT};
-
     private int getWhich(int id, int index) {
         switch (id) {
             case 1:
                 return IMAGE_OPTIONS[index];
-            case 2:
-                return VIDEO_OPTIONS[index];
-            case 3:
-                return FILE_OPTIONS[index];
-            case 4:
-                return AUDIO_OPTIONS[index];
         }
         return -1;
     }
