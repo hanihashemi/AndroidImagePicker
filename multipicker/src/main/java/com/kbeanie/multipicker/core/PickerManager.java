2822a5a8defd3474778c0be49b8d7c6ea0a8e831
hani <jhanihashemi@gmail.com>
2017-01-27 01:19:29 +0330

    Some cleaning

--- multipicker/src/main/java/com/kbeanie/multipicker/core/PickerManager.java
+++ multipicker/src/main/java/com/kbeanie/multipicker/core/PickerManager.java
@@ -28,28 +28,31 @@ import storage.StoragePreferences;
  * Abstract class for all types of Pickers
  */
 public abstract class PickerManager {
-    protected final int pickerType;
+    final int pickerType;
     protected Activity activity;
-    protected Fragment fragment;
-    protected android.app.Fragment appFragment;
-    protected int requestId;
+    private Fragment fragment;
+    private android.app.Fragment appFragment;
+    int requestId;
 
-    protected int cacheLocation = CacheLocation.EXTERNAL_STORAGE_PUBLIC_DIR;
+    int cacheLocation = CacheLocation.EXTERNAL_STORAGE_PUBLIC_DIR;
 
-    protected Bundle extras;
+    Bundle extras;
 
     protected boolean allowMultiple;
 
+    @SuppressWarnings("WeakerAccess")
     public PickerManager(Activity activity, int pickerType) {
         this.activity = activity;
         this.pickerType = pickerType;
     }
 
+    @SuppressWarnings("WeakerAccess")
     public PickerManager(Fragment fragment, int pickerType) {
         this.fragment = fragment;
         this.pickerType = pickerType;
     }
 
+    @SuppressWarnings("WeakerAccess")
     public PickerManager(android.app.Fragment appFragment, int pickerType) {
         this.appFragment = appFragment;
         this.pickerType = pickerType;
@@ -72,23 +75,21 @@ public abstract class PickerManager {
     /**
      * Triggers pick image
      *
-     * @return
      */
     protected abstract void pick() throws PickerException;
 
     /**
      * This method should be called after {@link Activity#onActivityResult(int, int, Intent)} is  called.
      *
-     * @param data
      */
     public abstract void submit(Intent data);
 
-    protected String buildFilePath(String extension, String type) throws PickerException {
+    String buildFilePath(String extension, String type) throws PickerException {
         String directoryPath = getDirectory(type);
         return directoryPath + File.separator + UUID.randomUUID().toString() + "." + extension;
     }
 
-    protected String getDirectory(String type) throws PickerException {
+    private String getDirectory(String type) throws PickerException {
         String directory = null;
         switch (cacheLocation) {
             case CacheLocation.EXTERNAL_STORAGE_PUBLIC_DIR:
@@ -108,7 +109,7 @@ public abstract class PickerManager {
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    protected Context getContext() {
+    Context getContext() {
         if (activity != null) {
             return activity;
         } else if (fragment != null) {
@@ -120,7 +121,7 @@ public abstract class PickerManager {
     }
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    protected void pickInternal(Intent intent, int type) {
+    void pickInternal(Intent intent, int type) {
         if (allowMultiple) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
                 intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
@@ -135,7 +136,7 @@ public abstract class PickerManager {
         }
     }
 
-    protected boolean isClipDataApi() {
+    boolean isClipDataApi() {
         return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN);
     }
 
@@ -143,22 +144,21 @@ public abstract class PickerManager {
         this.requestId = requestId;
     }
 
-    protected String getNewFileLocation(String extension, String type) throws PickerException {
+    String getNewFileLocation(String extension, String type) throws PickerException {
         File file;
         String filePathName = "";
-        if (type.equals(Environment.DIRECTORY_MOVIES)) {
-            filePathName = "movies";
-        } else if (type.equals(Environment.DIRECTORY_PICTURES)) {
+        if (type.equals(Environment.DIRECTORY_PICTURES)) {
             filePathName = "pictures";
         }
         file = new File(getContext().getFilesDir(), filePathName);
+        //noinspection ResultOfMethodCallIgnored
         file.mkdirs();
 
-        file = new File(file.getAbsolutePath() + File.separator + UUID.randomUUID().toString() + "." + extension);
+        file = new File(file.getAbsolutePath(), UUID.randomUUID().toString() + "." + extension);
         return file.getAbsolutePath();
     }
 
-    protected String getFileProviderAuthority() {
+    String getFileProviderAuthority() {
         return getContext().getPackageName() + ".multipicker.fileprovider";
     }
 }
